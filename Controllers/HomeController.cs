5a3b6bb2c7b30101c08423fcd177a7e3c4e3a268
Ford Henley <fordp86@gmail.com>
2023-03-17 17:36:24 -0500

    Completed Project
    Completed the FizzBuzz Project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -14,13 +14,47 @@ namespace FizzBuzz_MVC.Controllers
         }
 
         [HttpGet]
-
         public IActionResult FizzBuzzPage()
         {
             FizzBuzz model = new FizzBuzz();
 
             model.FizzValue = 3;
             model.BuzzValue = 5;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult FizzBuzzPage(FizzBuzz model)
+        {
+            List<string> fbItems = new List<string>();
+
+            bool Fizz;
+            bool Buzz;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Fizz = (i % model.FizzValue == 0);
+                Buzz = (i % model.BuzzValue == 0);
+
+                if (Fizz == true && Buzz == true)
+                {
+                    fbItems.Add("FizzBuzz");
+                }else if(Fizz == true)
+                {
+                    fbItems.Add("Fizz");
+                }else if(Buzz == true)
+                {
+                    fbItems.Add("Buzz");
+                }
+                else
+                {
+                    fbItems.Add(i.ToString());
+                }
+            }
+
+            model.Results = fbItems;
 
             return View(model);
         }
